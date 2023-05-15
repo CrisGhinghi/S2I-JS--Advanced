e6f1df8288dccc5e7891e0141ef3975f7bdec7c3
Cris <ghinghiloschicristina@gmail.com>
2023-05-15 15:39:19 +0200

    Update chart.js

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,18 +1,18 @@
 import Chart from 'chart.js/auto';
 
 export async function displayResults(data) {
-    // Recupero l'elemento immagine dell'header
+    // retrieve the header image element
     const headerImage = document.getElementById('header-image');
     headerImage.src = data.imageUrl;
 
-    // Creo il contenitore del grafico e dell'elemento canvas
+    // create the chart container and canvas element
     const resultsDiv = document.getElementById('results');
     const chartContainer = document.createElement('div');
     chartContainer.classList.add('chart-container');
     const chartCanvas = document.createElement('canvas');
     chartContainer.appendChild(chartCanvas);
 
-    // Mappo i dati delle categorie per il grafico
+    // map the category data for the chart
     const categories = data.categories.map(category => {
         return {
             label: category.name,
@@ -21,7 +21,7 @@ export async function displayResults(data) {
         };
     });
 
-    // Creo un grafico a barre con Chart.js
+    // create a bar chart with Chart.js
     const chart = new Chart(chartCanvas, {
         type: 'bar',
         data: {
@@ -52,13 +52,13 @@ export async function displayResults(data) {
         }
     });
 
-    // Creo l'elemento immagine della città
+    // create the city image element
     const cityImage = document.createElement('img');
     cityImage.src = data.imageUrl;
     cityImage.style.width = '100%';
     cityImage.style.height = 'auto';
 
-    // Imposto il contenuto HTML per i risultati della città
+    // set the HTML content for the city results
     resultsDiv.innerHTML = `
         <h1>${data.teleport_cityName}</h1>
         <h3>Teleport City Score</h3>
@@ -66,6 +66,6 @@ export async function displayResults(data) {
         <p>${data.summary}</p>
     `;
 
-    // Aggiungo il contenitore del grafico al div dei risultati
+    // add the chart container to the results div
     resultsDiv.appendChild(chartContainer);
 }
